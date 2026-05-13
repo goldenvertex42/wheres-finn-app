ed1c04dc19bf203cbc29bed9998a29c3cc056133
goldenvertex42 <fabled93@gmail.com>
2026-05-13 18:43:47 -0500

    feat: add characters route

--- backend/src/modules/game/game.routes.js
+++ backend/src/modules/game/game.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { startGame, validateClick } from './game.controller.js';
+import { getCharacters, startGame, validateClick, finishGame } from './game.controller.js';
 
 const router = Router();
 
+router.get('/characters', getCharacters);
 router.post('/start', startGame);
 router.post('/validate', validateClick);
+router.post('/finish', finishGame);
 
 export default router;
