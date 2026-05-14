befbe92b5e682ad6df4ffcfe3615f2520b9ea538
goldenvertex42 <fabled93@gmail.com>
2026-05-14 15:37:34 -0500

    fix(frontend): remove css that was making header always invisible
    - implement state and effect to check if the window is mobile or not, then combine that check with location check to return null and show only time when on game page on mobile

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,9 +1,20 @@
+import { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router";
 import styles from "./Header.module.css";
 
 const Header = () => {
   const location = useLocation();
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 600);
 
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth <= 600);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  if (isMobile && location.pathname === '/game') {
+    return null; 
+  }
   return (
     <header className={styles.header}>
       <h1>Where's Finn?</h1>
