2f20883aeb3086112726276de70fc0da7a6d0289
goldenvertex42 <fabled93@gmail.com>
2026-05-14 10:54:03 -0500

    refactor: App no longer manages any of the business logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,18 @@
-import { Outlet } from 'react-router'
-import { useState, useEffect } from 'react'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import './App.css'
+import { Outlet } from 'react-router';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import './App.css';
 
 function App() {
-  const [isTimerActive, setIsTimerActive] = useState(false);
-  const [time, setTime] = useState(0);
-
-  useEffect(() => {
-    let interval;
-
-    if (isTimerActive) {
-      const startTime = Date.now() - (time * 1000); // Resume from current time if needed
-
-      interval = setInterval(() => {
-        const now = Date.now();
-        // Calculate seconds elapsed and update state
-        setTime(Math.floor((now - startTime) / 1000));
-      }, 1000);
-    }
-
-    return () => clearInterval(interval);
-  }, [isTimerActive]);
-
-  const stopVisualTimer = () => setIsTimerActive(false);
   return (
     <div className='app-container'>
-      <Header time={Number(time)} />
+      <Header /> 
       <main>
-        <Outlet context={{ setIsTimerActive, setTime, stopVisualTimer }} />
+        <Outlet /> 
       </main>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
