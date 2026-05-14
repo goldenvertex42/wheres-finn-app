0105f74515922e6c7c26458b61dfd61179f05712
goldenvertex42 <fabled93@gmail.com>
2026-05-14 10:50:42 -0500

    refactor: remove legacy timer artifacts
    - with useGameSession, timer no longer being passed to Header from App
    - conditionally render Leaderboard or Restart link based on location

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,18 +1,14 @@
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import styles from "./Header.module.css";
 
-const Header = ({ time }) => {
-  const formatTime = (seconds) => {
-    const mins = Math.floor(seconds / 60).toString().padStart(2, '0');
-    const secs = (seconds % 60).toString().padStart(2, '0');
-    return `${mins}:${secs}`;
-  };
+const Header = () => {
+  const location = useLocation();
 
   return (
     <header className={styles.header}>
-      <h1>Where's Finn and His Friends?</h1>
-      <div className={styles.timerDisplay}>{formatTime(time)}</div>
-      <Link to={'/leaderboard'}>Leaderboard</Link>
+      <h1>Where's Finn?</h1>
+      {location.pathname === '/leaderboard' && <Link to={'/'} className={styles.link} state={null} >Try Again</Link>}
+      {location.pathname !== '/leaderboard' && <Link to={'/leaderboard'} className={styles.link} state={null} >Leaderboard</Link>}
     </header>
   );
 };
